34ecf3c894dc5126c7d8d72e779b0b7278803194
0033 <avachmansaxena@gmail.com>
2025-02-16 22:14:44 +0530

    commit1

---
+++ Feb2025/16Feb2025/Lab_0001.py
@@ -0,0 +1,10 @@
+# This is single line comment
+print("Restart python learning")
+print("on16th of Feb")
+print(2 + 2)
+print(2 / 2)
+print(2 - 2)
+print(2 * 2)
+print("recreate repo")
+print("recreate repo")
+print("recreate repo")
