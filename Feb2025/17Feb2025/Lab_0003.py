0066098006b60839b48909d72c2b5d44e5449a81
0033 <avachmansaxena@gmail.com>
2025-02-17 22:26:47 +0530

    commit Lab 0002 modification

---
+++ Feb2025/17Feb2025/Lab_0003.py
@@ -0,0 +1,11 @@
+print("Lab 0002")
+# self - concept of OOPS which point to itself
+# *args - unlimited numbers of Argument - string, int , float, boolean
+# sep=' ' - how you want to separate the arguments
+# end='\n' - in the end what you want to do
+# file=None - File IO
+
+print("hello", "World", 123, True, 31.67)
+print("hello", "World", 123, True, 31.67, sep='-#-')
+print("hello i am Achman", "i am new ", sep='-#-', end='\t')
+print("hello", "World", 123, True, 31.67, sep='-#-', end='&&&')
