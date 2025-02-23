45b413379eff8bdb1b373582143f7fce2311485c
0033 <avachmansaxena@gmail.com>
2025-02-23 23:30:04 +0530

    commit Lab 0002 modification

---
+++ Feb2025/17Feb2025/Lab_0004.py
@@ -0,0 +1,5 @@
+# Program to find maximum of 2 numbers
+
+print(max(10, 20))
+print(max(10, 20, 35, -90))
+print(max(10, 3.14, 19.23))
---
+++ Feb2025/17Feb2025/Lab_0005.py
@@ -0,0 +1,2 @@
+#to print the minimum value of the given number
+print(min(23.198, -998))
