28eb8252e736c5ec28c0c17929683575d85a330c
0033 <avachmansaxena@gmail.com>
2025-02-24 00:06:28 +0530

    commit Lab 0002 modification

---
+++ Feb2025/17Feb2025/Lab_0008.py
@@ -0,0 +1,33 @@
+# variable name should be start grom A-Z, a-z _
+name = "Pramod"
+print(type(name))
+
+# variable name should not start with number
+# 123 =345 - variable names
+
+
+# variable name should not be any keyword
+# keyword ? // Reserved word
+# 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield' [2]
+# and = 123
+
+# variable name should not be any special character
+# @ = 123
+# $ = 344
+_ = 123
+print(_)
+_name = "pramod"
+print(_name)
+# $prampod = "pramod"
+# pramod$ = "Hello"
+
+# variable name should not be any space
+# first name = "Pramod"
+first_name = "Pramod"
+print(first_name)
+# Python love the snake case
+varaible_name = "pramod"
+print(varaible_name)
+
+long_var_name_is_created_here = "Hello"
+print(long_var_name_is_created_here)
