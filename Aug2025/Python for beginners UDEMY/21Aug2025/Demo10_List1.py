9555429e4de9f1ab84846f24f83416d3fdcbf6b8
0033 <avachmansaxena@gmail.com>
2025-08-23 18:47:12 +0530

    Merge remote-tracking branch 'origin/master'

--- Aug2025/Python for beginners UDEMY/21Aug2025/Demo10_List1.py
+++ Aug2025/Python for beginners UDEMY/21Aug2025/Demo10_List1.py
@@ -7,5 +7,25 @@ print(lst2)
 
 print(lst2[3])
 print(lst2[2:5])
-print(lst2*3)
+print(lst2 * 3)
+lst1.append(56.78)  # to append the list
+print(lst1)
+lst2.remove(10)  # remove by value
+print(lst2)
+del (lst2[2])
+print(lst2)
 
+#   lst1.clear()    # to clear the data in the list
+#   print(lst1)
+
+
+print(max(lst2))
+print(min(lst2))
+lst2.insert(5, 234)
+print(lst2)
+
+lst2.sort()
+print(lst2)
+
+lst2.sort(reverse=True)
+print(lst2)
