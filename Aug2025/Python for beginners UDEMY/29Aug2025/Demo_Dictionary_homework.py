169f6fa3f7cd2dd1535305ce80e2713b497a568e
0033 <avachmansaxena@gmail.com>
2026-01-28 16:52:33 +0530

    changes 28 jan 2026

---
+++ Aug2025/Python for beginners UDEMY/29Aug2025/Demo_Dictionary_homework.py
@@ -0,0 +1,15 @@
+student_info = {'name': 'Achman', 'age': 32, 'major': 'Engineering'}
+
+print(student_info)
+print(student_info['name'])
+print(student_info['age'])
+
+student_info['email'] = 'anubhav.23@gmail.com'
+print(student_info['email'])
+
+student_info['age'] = 22
+print(student_info['age'])
+print(student_info)
+
+del student_info['major']
+print(student_info)
