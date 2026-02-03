c5b1e89d172a9a25718738a50dc798427428c344
0033 <avachmansaxena@gmail.com>
2026-02-03 11:53:53 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	Python for beginners UDEMY/29Aug2025/Demo_Dictionary_homework.py

--- Python for beginners UDEMY/1Feb2026/Demo30_InputFun3.py
+++ Python for beginners UDEMY/1Feb2026/Demo30_InputFun3.py
@@ -1,2 +1,6 @@
-lst = [x for x in input("Enter three numbers separated by space : ").split()]
-print(lst)
+lst1 = [x for x in input("Enter three numbers separated by commas : ").split(",")]
+print(" this is the list" ,lst1)
+lst2 = [int(x) for x in input("Enter three integers numbers separated by commas : ").split(",")]
+print(" this is the list of integers" ,lst2)
+lst3 = [float(x) for x in input("Enter three floating separated by commas : ").split(",")]
+print(" this is the list of floating numbers" ,lst3)
